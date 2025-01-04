1dba8db219bd255b76593cea4c1580bcf201fc87
BugHunter-X <askzenai@gmail.com>
2025-01-04 19:48:05 +0100

    Add bugSolution.cob file

---
+++ bugSolution.cob
@@ -0,0 +1,17 @@
+01  WS-AREA. 
+    05  WS-DATE PIC 9(6). 
+    05  WS-TIME PIC 9(6). 
+    05  WS-DATE-COMP PIC 9(6). 
+
+PROCEDURE DIVISION. 
+    DISPLAY "Enter date (YYYYMMDD): ". 
+    ACCEPT WS-DATE. 
+    DISPLAY "Enter time (HHMMSS): ". 
+    ACCEPT WS-TIME. 
+
+    MOVE 20231026 TO WS-DATE-COMP 
+
+    IF WS-DATE < WS-DATE-COMP THEN 
+       DISPLAY "Date is before 20231026" 
+    END-IF. 
+    STOP RUN.
